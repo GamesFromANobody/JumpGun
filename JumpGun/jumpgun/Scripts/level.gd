9f8a77d7971f7fb39e09bbcb2a2a30c8e14bd77d
GamesFromANobody <156671338+GamesFromANobody@users.noreply.github.com>
2025-01-18 21:31:46 +0100

    Add files via upload

---
+++ JumpGun/jumpgun/Scripts/level.gd
@@ -0,0 +1,19 @@
+extends Node2D
+#This node should control pausing and game speed (if we want to change it)
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(_delta: float) -> void:
+	pass
+
+func Pause():
+	pass
+
+func Unpause():
+	pass
+
+func ChangeLevel():
+	pass
