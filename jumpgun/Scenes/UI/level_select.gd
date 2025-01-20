97055b68aebb50318063be771ce6a25f308e50e2
GamesFromANobody <156671338+GamesFromANobody@users.noreply.github.com>
2025-01-20 20:36:03 +0100

    Add files via upload

--- jumpgun/Scenes/UI/level_select.gd
+++ jumpgun/Scenes/UI/level_select.gd
@@ -1,9 +1,15 @@
 extends Control
 
+@export var level_path : String = "res://Scenes/Levels/level_test.tscn"
 
+var levelList : Array[String] = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for file in DirAccess.open("res://Scenes/Levels/").get_files():
+		print(file)
+		$AspectRatioContainer/ItemList.add_item(str(file))
+		levelList.append(str(file))
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,3 +27,10 @@ func _on_testing_pressed() -> void:
 
 func _on_testing_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/level_grodis1.tscn")
+
+
+
+func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	var fileToLoad = "res://Scenes/Levels/" + levelList[index - 1]
+	print(fileToLoad)
+	get_tree().change_scene_to_file(fileToLoad)
