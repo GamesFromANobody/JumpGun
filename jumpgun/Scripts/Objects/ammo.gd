3e0114f1b673e1f2ccca0dc294fd3a9bc5402e90
GamesFromANobody <156671338+GamesFromANobody@users.noreply.github.com>
2025-01-21 10:59:47 +0100

    Reorginize file structure

--- jumpgun/Scripts/Objects/ammo.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-class_name Ammo
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		if body.reloadAmmo():
-			queue_free()
--- jumpgun/Scripts/Objects/target.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody2D
-class_name Target
-
-signal targetHit()
-
-func Hit():
-	targetHit.emit()
-	queue_free()
