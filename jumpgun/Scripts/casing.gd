af3a0b935c8d2fd9b80a9843607f11b3f60dc85d
GamesFromANobody <156671338+GamesFromANobody@users.noreply.github.com>
2025-01-24 19:56:48 +0100

    Animation update

---
+++ jumpgun/Scripts/casing.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+
+var scaleSet = Vector2(1, 1)
+
+func changeScale(newScale : Vector2):
+	scaleSet = newScale
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	scale = scaleSet
