1be34b2610f79e6c1503650fe7fdf35bcf093098
GamesFromANobody <156671338+GamesFromANobody@users.noreply.github.com>
2025-01-31 14:37:35 +0100

    FINAL UPDATE

--- jumpgun/Scripts/music_controller.gd
+++ jumpgun/Scripts/music_controller.gd
@@ -7,14 +7,14 @@ extends AudioStreamPlayer
 @export var muteMusic = false
 
 var tracks = [ 
-	preload("res://Import/Audio/Music/Maja Salamon - Cute Gun In-Game Music 1 WIP 7 (LOOP).wav"),
-	preload("res://Import/Audio/Music/Maja Salamon - Layer 1 - Win - Loss - Level Select (LOOP).wav"),
-	preload("res://Import/Audio/Music/Maja Salamon - Layer 2 Loss (LOOP).wav"),
-	preload("res://Import/Audio/Music/Maja Salamon - Layer 2 Loss (LOOP).wav"),
-	preload("res://Import/Audio/Music/Maja Salamon - Layer 2 Loss (LOOP).wav"),
-	preload("res://Import/Audio/Music/Maja Salamon - Layer 2 Loss (LOOP).wav"),
-	preload("res://Import/Audio/Music/Maja Salamon - Layer 2 Win (LOOP).wav"),
-	preload("res://Import/Audio/Music/Maja Salamon - Layer 1 - Win - Loss - Level Select (LOOP).wav"),
+	preload("res://Import/Audio/Music/Maja Salamon - Menu (GUNZO OST) (Menu) (LOOP).wav"),
+	preload("res://Import/Audio/Music/Maja Salamon - Ah, Shoot! (GUNZO OST) (Level 1) (LOOP).wav"),
+	preload("res://Import/Audio/Music/Maja Salamon - Need More Ammo! (GUNZO OST) (Level 2) (LOOP).wav"),
+	preload("res://Import/Audio/Music/Maja Salamon - Pew Pew (GUNZO OST) (Level 3) (LOOP).wav"),
+	preload("res://Import/Audio/Music/Maja Salamon - Gun on the Run (GUNZO OST) (Level 4) (LOOP).wav"),
+	preload("res://Import/Audio/Music/Maja Salamon - Blasphemy! (GUNZO OST) (Level 5) (LOOP).wav"),
+	preload("res://Import/Audio/Music/Maja Salamon - Gnumzo (GUNZO OST) (Level 6) (LOOP).wav"),
+	preload("res://Import/Audio/Music/Maja Salamon - Gnomed (GUNZO OST) (Level 6 Easter Egg) (LOOP).wav"),
 	]
 
 func _ready() -> void:
